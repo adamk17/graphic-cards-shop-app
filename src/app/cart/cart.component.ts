dea1d303b3f77e6e65e209e292624190ab304747
Adam Kucharski <adam.kucharski@knowit.eu>
2023-04-27 19:21:16 +0200

    Add sum of Total price in cart and button in cads list

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -9,6 +9,7 @@ import { CartService } from '../cart.service';
 export class CartComponent {
 
   items = this.cartService.getItems();
+  totalPrice = this.cartService.getTotalPrice();
 
   constructor(
     private cartService: CartService
