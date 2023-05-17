532f9a9f53ca3aefdeaaf456608109390e9eb880
adamk17 <adam,kucharski17@gmail.com>
2023-05-17 23:12:19 +0200

    validation added

--- src/app/adress-field/adress-field.component.ts
+++ src/app/adress-field/adress-field.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder } from '@angular/forms';
 import { Location } from '@angular/common';
 import { CartService } from '../cart.service'
 import { ShippingService } from '../shipping.service'
-import {PickupPointService} from '../pickup-point.service'
+import { PickupPointService } from '../pickup-point.service'
 import { Shipping } from '../shipping';
 import { InPostParcelLockers } from '../inPostParcelLockers';
 import { DomSanitizer } from '@angular/platform-browser';
@@ -14,7 +14,7 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./adress-field.component.css']
 })
 export class AdressFieldComponent implements OnInit {
-  
+
   showB = true;
   chosenShipping!: Shipping;
   id = this.shippingService.getChosenShippingId();
@@ -28,16 +28,15 @@ export class AdressFieldComponent implements OnInit {
     'assets/visa.png',
     'assets/mastercard.png'
   ];
-  
+
 
   constructor(
     private formBuilder: FormBuilder,
     private cartService: CartService,
     private shippingService: ShippingService,
     private pickupPointService: PickupPointService,
-    private sanitizer: DomSanitizer
-    ) {}
-  
+  ) { }
+
 
   onCloseClicked() {
     this.showB = false;
@@ -51,51 +50,83 @@ export class AdressFieldComponent implements OnInit {
 
   ngOnInit(): void {
     this.chosenShipping = this.shippingService.getChosenShipping();
-    
+
   }
 
-  
 
-    checkoutForm = this.formBuilder.group({
-      name: '',
-      surname: '',
-      country: '',
-      city: '',
-      postcode: '',
-      streetName: '',
-      streetNumber: '',
-    });
 
-    onSubmit(): void {
-      // Process checkout data here
-      let name = this.checkoutForm.value.name;
-      let surname = this.checkoutForm.value.surname;
-      let country = this.checkoutForm.value.country;
-      let city = this.checkoutForm.value.city;
-      let postcode = this.checkoutForm.value.postcode;
-      let streetname = this.checkoutForm.value.streetName;
-      let streetnumber = this.checkoutForm.value.streetNumber;
-      window.alert('name: ' + name + ' Surname: ' + surname + " \nadress " + country + city + postcode + streetname + streetnumber);
-      this.checkoutForm.reset();
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    surname: '',
+    email: '',
+    pesel: '',
+    country: '',
+    city: '',
+    postcode: '',
+    streetName: '',
+    streetNumber: '',
+  });
+
+  onSubmit(): void {
+    // Process checkout data here
+    let name = this.checkoutForm.value.name;
+    let surname = this.checkoutForm.value.surname;
+    let email = this.checkoutForm.value.email;
+    let pesel = this.checkoutForm.value.pesel;
+    let country = this.checkoutForm.value.country;
+    let city = this.checkoutForm.value.city;
+    let postcode = this.checkoutForm.value.postcode;
+    let streetname = this.checkoutForm.value.streetName;
+    let streetnumber = this.checkoutForm.value.streetNumber;
+
+    if (this.canDisplayAdress()) {
+      if (this.validatePersonalData() && this.validateAdress()) {
+        window.alert('name: ' + name + ' Surname: ' + surname +
+          " \nadress " + country + city + postcode + streetname + streetnumber +
+          "\image " + this.chosenImage);
+          this.checkoutForm.reset();
+      }
+      else {
+        window.alert('bad data')
+        //this.checkoutForm.reset();
+      }
+
+    }
+    else if (!this.canDisplayAdress()) {
+      if (this.validatePersonalData()) {
+         window.alert('name: ' + name + ' Surname: ' + surname +
+          "\image " + this.chosenImage);
+          this.checkoutForm.reset();
+      }
+      else {
+        window.alert('bad data')
+        //this.checkoutForm.reset();
+      }
+    }
+    else {
+      window.alert('bad data')
+      //this.checkoutForm.reset();
     }
 
-    canDisplayMap() {
-      return ( (this.showB == true) && (this.id == 12 || this.id == 15 || this.id == 24));
-    }
+  }
 
-    canDisplayAdress() {
-      return (this.id == 11 || this.id == 13 || this.id == 14 || this.id == 21 || this.id == 22 || this.id == 23);
-    }
+  canDisplayMap() {
+    return ((this.showB == true) && (this.id == 12 || this.id == 15 || this.id == 24));
+  }
 
-    canDisplayDeliveryPoint() {
-      return ((this.id == 12 || this.id == 15 || this.id == 24) && this.chosenParcel);
-    }
+  canDisplayAdress() {
+    return (this.id == 11 || this.id == 13 || this.id == 14 || this.id == 21 || this.id == 22 || this.id == 23);
+  }
 
-    getAllPrice() {
-      return this.cartService.getTotalPrice() + this.shippingService.getChosenShippingPrice();
-    }
+  canDisplayDeliveryPoint() {
+    return ((this.id == 12 || this.id == 15 || this.id == 24) && this.chosenParcel);
+  }
 
-    
+  getAllPrice() {
+    return this.cartService.getTotalPrice() + this.shippingService.getChosenShippingPrice();
+  }
+
+
   currentIndex = 0;
 
   get currentImage(): string {
@@ -114,33 +145,141 @@ export class AdressFieldComponent implements OnInit {
     this.chosenImage = image;
   }
 
+  validatePesel(pesel: string) {
+    if (pesel.length !== 11) {
+      return false;
+    }
+    //window.alert('dlugosc pesel w porządku');
 
-  
+    const weights = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1];
+    let sum = 0;
+    for (let i = 0; i < 11; i++) {
+      const digit = parseInt(pesel.charAt(i), 10);
+      if (isNaN(digit)) {
+        return false;
+      }
+      sum += digit * weights[i];
+    }
+    //window.alert(' pesel zawiera cyfry');
+
+
+
+    window.alert('pesel dobry');
+    return true;
+  }
+
+  validateZipCode(zipCode: string) {
+    if (zipCode.length !== 6) {
+      return false;
+    }
+    //window.alert('kod pocztowy ma prawidłową długość');
+
+    const firstTwoDigits = zipCode.substring(0, 2);
+    if (isNaN(Number(firstTwoDigits)) || Number(firstTwoDigits) < 1 || Number(firstTwoDigits) > 99) {
+      return false;
+    }
+    //window.alert('kod pocztowy zawiera prawidlowe cyfry z przodu');
+
+    if (zipCode.charAt(2) !== '-') {
+      return false;
+    }
+    //window.alert('kod pocztowy zawiera -');
+
+    const lastThreeDigits = zipCode.substring(3);
+    if (isNaN(Number(lastThreeDigits))) {
+      return false;
+    }
+    //window.alert('kod pocztowy zawiera  cyfry z tyłu');
+
+    //window.alert('pkod pocztowy w porządku')
+    return true;
+  }
+
+  validatePersonalData() {
+    window.alert('cos sie dzieje');
+    let name = this.checkoutForm.value.name;
+    let surname = this.checkoutForm.value.surname;
+    let email = this.checkoutForm.value.email;
+    let pesel = this.checkoutForm.value.pesel;
+
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!name) return false;
+    else if (name === '') return false;
+    //window.alert('poszlo imie');
+
+    if (!surname) return false;
+    else if (surname === '') return false;
+    //window.alert('poszlo nazwisko');
+
+    if (!email) return false;
+    else if (email === '') return false;
+    else if (!emailRegex.test(email)) return false;
+    //window.alert('poszlo email');
+
+
+    if (!pesel) return false;
+    else if (pesel === '') return false
+    else if (!this.validatePesel(pesel)) return false;
+    //window.alert('poszlo pesel');
+
+    return true;
+
+  }
+
+  validateAdress() {
+    let country = this.checkoutForm.value.country;
+    let city = this.checkoutForm.value.city;
+    let postcode = this.checkoutForm.value.postcode;
+    let streetname = this.checkoutForm.value.streetName;
+    let streetnumber = this.checkoutForm.value.streetNumber;
+
+    if (!country) return false;
+    else if (country === '') return false;
+
+    if (!city) return false;
+    else if (city === '') return false;
+
+    if (!postcode) return false;
+    if (!this.validateZipCode(postcode)) return false;
+
+    if (!streetname) return false;
+    else if (streetname === '') return false;
+
+    if (!streetnumber) return false;
+    else if (streetnumber === '') return false;
+
+    return true;
+  }
+
+
+
   //chuj ci w dupe pietek
-// id: any;
-// textToDisplay: string = " Chuj ci w dupe pietek";
-// menuVisible: boolean = false;
-// userVisible: boolean = false;
+  // id: any;
+  // textToDisplay: string = " Chuj ci w dupe pietek";
+  // menuVisible: boolean = false;
+  // userVisible: boolean = false;
 
-// animateText() {
-// const mainText = document.getElementById("main_text");
-// let textIterator = 0;
-// let num = this.id;
-// clearInterval(this.id!);
-// this.id = setInterval(() => {
+  // animateText() {
+  // const mainText = document.getElementById("main_text");
+  // let textIterator = 0;
+  // let num = this.id;
+  // clearInterval(this.id!);
+  // this.id = setInterval(() => {
 
- 
-//   if (textIterator != this.textToDisplay.length)
-//     {
-//       textIterator++;
-//       mainText!.innerText = this.textToDisplay.substring(0, textIterator);
-//       mainText!.style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-//       mainText!.style.fontSize = (Math.floor(Math.random() * 5) + 98) + "px";
-  
-//     }
-//     else textIterator = 0;
-//   }, 200);
-//}
+
+  //   if (textIterator != this.textToDisplay.length)
+  //     {
+  //       textIterator++;
+  //       mainText!.innerText = this.textToDisplay.substring(0, textIterator);
+  //       mainText!.style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  //       mainText!.style.fontSize = (Math.floor(Math.random() * 5) + 98) + "px";
+
+  //     }
+  //     else textIterator = 0;
+  //   }, 200);
+  //}
 
 
 
--- src/app/cards.ts
+++ src/app/cards.ts
@@ -5,6 +5,9 @@ export interface Cards {
     cardLength: number;
     ram: number;
     coreClock: number;
+    coreClockInBoost: number;
+    resolution: number;
+    led: boolean;
     description: string;
     amount: number;
     image: string;
@@ -15,11 +18,14 @@ export const cards = [
     {
         id: 1,
         name: 'Rtx 3070',
-        price: 3000,
-        cardLength: 230,
-        ram: 16,
-        coreClock: 1800,
-        description: 'this is good card',
+        price: 2500,
+        cardLength: 267,
+        ram: 8,
+        coreClock: 1500,
+        coreClockInBoost: 1846,
+        resolution: 4320,
+        led: true,
+        description: 'this is good card, you can play new games on it, but sometimes not on max resolution',
         amount: 0,
         image: 'assets/card pictures/rtx 3070.jpg'
     },
@@ -27,10 +33,13 @@ export const cards = [
         id: 2,
         name: 'Rtx 3080 ti',
         price: 6000,
-        cardLength: 250,
-        ram: 24,
-        coreClock: 2000,
-        description: 'this is realy good card',
+        cardLength: 300,
+        ram: 16,
+        coreClock: 1700,
+        coreClockInBoost: 2350,
+        resolution: 4320,
+        led: true,
+        description: 'this is really good card, you can play all new games, mostly on max resolution',
         amount: 0,
         image: 'assets/card pictures/rtx 3080 ti.jpg'
     },
@@ -38,10 +47,13 @@ export const cards = [
         id: 3,
         name: 'Rtx 4090',
         price: 8000,
-        cardLength: 220,
-        ram: 32,
-        coreClock: 2100,
-        description: 'you can play almost everything',
+        cardLength: 340,
+        ram: 24,
+        coreClock: 1900,
+        coreClockInBoost: 2550,
+        resolution: 4320,
+        led: true,
+        description: 'on this beast you can play almost everything',
         amount: 0,
         image: 'assets/card pictures/rtx 4090.jpg'
     },
@@ -51,8 +63,39 @@ export const cards = [
         price: 3000,
         cardLength: 240,
         ram: 8,
+        coreClock: 1300,
+        coreClockInBoost: 1700,
+        resolution: 2160,
+        led: false,
+        description: 'you can buy it and play some simple games',
+        amount: 0,
+        image: 'assets/card pictures/rtx 3060.jpg'
+    },
+    {
+        id: 5,
+        name: 'Rtx 2080 ti',
+        price: 2000,
+        cardLength: 250,
+        ram: 10,
+        coreClock: 1600,
+        coreClockInBoost: 1900,
+        resolution: 4320,
+        led: true,
+        description: 'you can buy it and play some litle old games',
+        amount: 0,
+        image: 'assets/card pictures/rtx 3060.jpg'
+    },
+    {
+        id: 6,
+        name: 'Gtx 1600',
+        price: 999,
+        cardLength: 280,
+        ram: 6,
         coreClock: 1500,
-        description: '',
+        coreClockInBoost: 1860,
+        resolution: 2160,
+        led: true,
+        description: 'you can play some old games on it',
         amount: 0,
         image: 'assets/card pictures/rtx 3060.jpg'
     },
